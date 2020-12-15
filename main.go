a52c98c9e556ccdaf6e7e8a8bdb95d77ad6a4efb
An Duong <andyduong1920@gmail.com>
2020-12-15 17:12:39 +0700

    Handle error case

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	_ "goMarketer/models"
 	_ "goMarketer/routers"
@@ -12,12 +13,22 @@ import (
 )
 
 func init() {
-	orm.RegisterDriver("postgres", orm.DRPostgres)
-	orm.RegisterDataBase("default", "postgres", beego.AppConfig.String("db_url"))
+	driverErr := orm.RegisterDriver("postgres", orm.DRPostgres)
+
+	if driverErr != nil {
+		log.Fatal(fmt.Sprintf("Failed to register the driver %v", driverErr))
+	}
+
+	databaseErr := orm.RegisterDataBase("default", "postgres", beego.AppConfig.String("db_url"))
+
+	if databaseErr != nil {
+		log.Fatal(fmt.Sprintf("Failed to connect to the database %v", databaseErr))
+	}
 }
 
 func main() {
 	err := orm.RunSyncdb("default", false, true)
+
 	if err != nil {
 		fmt.Println(err)
 	}
